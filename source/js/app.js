c0fbd36f66a6116e35483cd28cd98309e89e6c9e
Dominik <domi38h@gmail.com>
2018-05-31 13:26:49 +0200

    bugfixing

--- source/js/app.js
+++ source/js/app.js
@@ -33,67 +33,67 @@ smoothScrollWithoutHash( 'a[href*="#"]', {
 // Vue
 // ##########################################################################
 
-var tabs = new Vue({
-	el: '#tabs',
-	data: {
-		tabsOpen: false,
-		activeTab: 1,
-		windowWidth: 0
-	},
-	methods: {
-		getWindowWidth: function(event) {
-			this.windowWidth = document.documentElement.clientWidth;
-		},
-		closeTab: function() {
-			this.tabsOpen = false;
-		},
-		openTab: function() {
-			this.tabsOpen = true;
-		},
-		changeTab: function(index) {
-			if(this.activeTab > index) {
-				this.$el.classList.add('reverse');
-				this.activeTab = index;
-			} else if(this.activeTab < index && this.$el.classList.contains('reverse')) {
-				this.$el.classList.remove('reverse');
-				this.activeTab = index;
-			} else {
-				this.activeTab = index;
-			}
-		},
-		tabClick: function(index) {
-			if(this.tabsOpen) {
-				if(this.activeTab == index) {
-					this.closeTab();
-				} else {
-					this.changeTab(index);
-				}
-			} else {
-				this.changeTab(index);
-				this.openTab();
-			}
-		}
-	},
-	computed: {
-		small: function() {
-			if(this.windowWidth >= 1180) {
-				return false;
-			} else {
-				return true;
-			}
-		}
-	},
-	mounted: function() {
-		this.$nextTick(function() {
-			window.addEventListener('resize', this.getWindowWidth);
-			window.addEventListener('resize', this.calculateCoords);
+// var tabs = new Vue({
+// 	el: '#tabs',
+// 	data: {
+// 		tabsOpen: false,
+// 		activeTab: 1,
+// 		windowWidth: 0
+// 	},
+// 	methods: {
+// 		getWindowWidth: function(event) {
+// 			this.windowWidth = document.documentElement.clientWidth;
+// 		},
+// 		closeTab: function() {
+// 			this.tabsOpen = false;
+// 		},
+// 		openTab: function() {
+// 			this.tabsOpen = true;
+// 		},
+// 		changeTab: function(index) {
+// 			if(this.activeTab > index) {
+// 				this.$el.classList.add('reverse');
+// 				this.activeTab = index;
+// 			} else if(this.activeTab < index && this.$el.classList.contains('reverse')) {
+// 				this.$el.classList.remove('reverse');
+// 				this.activeTab = index;
+// 			} else {
+// 				this.activeTab = index;
+// 			}
+// 		},
+// 		tabClick: function(index) {
+// 			if(this.tabsOpen) {
+// 				if(this.activeTab == index) {
+// 					this.closeTab();
+// 				} else {
+// 					this.changeTab(index);
+// 				}
+// 			} else {
+// 				this.changeTab(index);
+// 				this.openTab();
+// 			}
+// 		}
+// 	},
+// 	computed: {
+// 		small: function() {
+// 			if(this.windowWidth >= 1180) {
+// 				return false;
+// 			} else {
+// 				return true;
+// 			}
+// 		}
+// 	},
+// 	mounted: function() {
+// 		this.$nextTick(function() {
+// 			window.addEventListener('resize', this.getWindowWidth);
+// 			window.addEventListener('resize', this.calculateCoords);
 
-			//Init
-			this.getWindowWidth();
-		})
-	},
-	beforeDestroy: function() {
-		window.removeEventListener('resize', this.getWindowWidth);
-		window.removeEventListener('resize', this.calculateCoords);
-	}
-});
+// 			//Init
+// 			this.getWindowWidth();
+// 		})
+// 	},
+// 	beforeDestroy: function() {
+// 		window.removeEventListener('resize', this.getWindowWidth);
+// 		window.removeEventListener('resize', this.calculateCoords);
+// 	}
+// });
